5c189e405bc4a6e00170953fb0095b0246c1aa75
Sunil <sunil@incedoinc.com>
2020-12-03 13:23:24 +0530

    added comment on test cases

--- cypress/integration/demoTest/api_test.spec.js
+++ cypress/integration/demoTest/api_test.spec.js
@@ -1,12 +1,22 @@
 /// <reference types="cypress"/>
 
 context('CRUD API test', () =>{
+    //Testing the get api checking the url
     it('Get API Test', ()=>{
         cy.request('GET', 'https://httpbin.org/get').then((response) =>{
+            //checking the 200 status
+            expect(response).to.have.property('status',200)
+
+            //checking duration key in response
+            expect(response).to.have.property('duration')
+            cy.log(response.duration);
+
+            //checking the url key is getting in the response body
             expect(response.body).to.have.property('url','https://httpbin.org/get')
         })
     })
 
+    //Testing the post api by creating new user
     it('Post API test', ()=>{
         cy.request({
             method: "POST",
@@ -16,29 +26,69 @@ context('CRUD API test', () =>{
             },
             body: {
                 "name":"Pramod",
+                "age": 28,
+                "designation": "Senior Software Engineer"                
             }
         }).then((response) =>{
+            // checking the server name from headers
+            expect(response.headers).to.have.property('server');
+            expect(response.headers).to.have.property('server',"gunicorn/19.9.0");
+
+            // checking the json key getting in case fo success 
             expect(response.body).to.have.property('json');
-            expect(response.body.json).to.deep.equal({'name':'Pramod'})
+            
+            // checking the successfully getting the name of user after get created the user.
+            expect(response.body.json).to.deep.equal({'name':'Pramod', "age":28, "designation":'Senior Software Engineer'})
         });
     });
 
+    //Testing the put api 
     it('Put API Test', ()=>{
-        cy.request('PUT','https://httpbin.org/put', {'name':'Pramod'}).then((response) =>{
+        cy.request('PUT','https://httpbin.org/put', {'name':'Pramod Sharma'}).then((response) =>{
+            // checking the json key getting in case fo success
             expect(response.body).to.have.property('json');
-            expect(response.body.json).to.deep.equal({'name':'Pramod'});
+
+            // checking name is updated successfully or not
+            expect(response.body.json).to.deep.equal({'name':'Pramod Sharma'});
         });
     });
+    //Testing the patch api is working properly or not.
     it('Patch API Test',()=>{
-        cy.request('PATCH', 'https://httpbin.org/patch', {'name':'Pramod'}).then((response) =>{
+        cy.request('PATCH', 'https://httpbin.org/patch', {'name':'Pramod Kumar'}).then((response) =>{
+            // checking the json key getting in case fo success
             expect(response.body).to.have.property('json');
-            expect(response.body.json).to.deep.equal({'name':'Pramod'})
+
+            // checking name is updated successfully or not
+            expect(response.body.json).to.deep.equal({'name':'Pramod Kumar'})
         });
     });
+    //Testing the delete api
     it('Delete API Test',()=>{
         cy.request('DELETE', 'https://httpbin.org/delete').then((response) =>{
+            //checking the url key is getting in the response body
             expect(response.body).to.have.property('url','https://httpbin.org/delete')
         });
     });
+
+    //Getting the data url of image 
+    it('Data url of an image', ()=>{
+        cy.request({
+            url: 'https://docs.cypress.io/img/logo.png',
+            encoding: 'base64',
+          })
+          .then((response) => {
+
+            //testing getting success or not
+            expect(response).to.have.property('status',200)
+
+            const Imagebase64Content = response.body
+
+            // getting content-type from header
+            const mimetype = response.headers['content-type'] 
+
+            //creating data url of image
+            const imageDataUrl = `data:${mimetype};base64,${Imagebase64Content}`
+          })
+    })
 });
 
--- cypress/integration/demoTest/login_flow.spec.js
+++ cypress/integration/demoTest/login_flow.spec.js
@@ -4,21 +4,33 @@ describe("This is login test", ()=>{
     beforeEach(() =>{
         cy.visit("https://s2.demo.opensourcecms.com/orangehrm/symfony/web/index.php/auth/login")
     });
+    //checking the successfully login  flow
     it('Login Success flow', ()=>{
         cy.get('#txtUsername').type("opensourcecms").should('have.value','opensourcecms')
         cy.get('#txtPassword').type("opensourcecms").should('have.value','opensourcecms')
         cy.get('#btnLogin').click();
+
+        //checking the successfully redirected on the url after logged in
         cy.url().should('include', 'orangehrm/index.php')
+
+        //checking the user successfully logged in or not
         cy.get('#option-menu > li:nth-child(1)').then(($text1) =>{
             const welcometext = $text1.text(); 
             expect(welcometext).to.match(/Welcome .+/)
         });
     });
+    //checking the failure login flow
     it("Login Failure Flow", ()=>{
         cy.get('#txtUsername').type("opensourcecms").should('have.value','opensourcecms')
+
+        //enter the wrong password
         cy.get('#txtPassword').type("opensourcecms6").should('have.value','opensourcecms6')
         cy.get('#btnLogin').click();
+
+        //checking in case of login failure user redirected on the validatecredentials page of not
         cy.url().should('include', '/auth/validateCredentials')
+
+        //error message should be visible in case of invalid credentials
         cy.get('#spanMessage').should('be.visible');
     });
 });
